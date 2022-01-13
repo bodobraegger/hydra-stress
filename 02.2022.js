2f073748b015c286e1a3da88f5a4f6e486a2bdb1
broesmel <23656696+broesmel@users.noreply.github.com>
2022-01-13 13:54:42 +0100

    Update 02.2022.js

--- 02.2022.js
+++ 02.2022.js
@@ -9,3 +9,14 @@ src(o0).modulateHue(src(o0).scale(1.01),1).hue(0.003)
 
 // fade out gracefully
 src(o0).mult(shape(4).scale(2.5)).scale(0.92).rotate(1e-2).out()
+
+shape(3).color(() => Math.tan(time), () => Math.cos(time), () => Math.sin(time), () => Math.arctan(time)).out(o1)
+//dimming screen
+src(o0).scale(1.01).rotate(0.01).modulateHue(o0).blend(o1,0.1).diff(o0,0.001).out()
+osc(105, 0.01, 0).thresh().modulateRotate(noise(10000).thresh()).modulateScale(osc(105, 0.01).rotate(()=>Math.sin(time)).mask(shape(3))).diff(shape(4, 0.9).color(0,0,1).diff(solid().invert().color(1,1,0)).invert()).out(o2)
+src(o0).blend(o2).out(o3)
+
+src(o3).scale(1.01).rotate(0.01).modulateHue(o3).mult(o3,0.001).out(o3)
+
+
+src(o3).out(o3)
