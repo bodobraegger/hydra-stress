e811fd26a89bf191313103050f3b3b068c5cb3a0
bodobraegger <braegger.bodo@gmail.com>
2025-03-24 18:05:25 +0100

    feat: update to use punchcard instead of pianoroll

--- 24.03.25@zhdk-comp-task-1.js
+++ 24.03.25@zhdk-comp-task-1.js
@@ -1,5 +1,4 @@
-// https://strudel.cc/#LyoqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqKioqLwogIHN0YWNrKAogICAgcygid2hpdGUgfCBwaW5rIHwgYnJvd24iKS5hZHNyKCIuMTouMTowOi4yIikuc3VzdGFpbihwZXJsaW4ucmFuZ2UoLjQsIC42KSkuZ2FpbiguMykKICAgICAgLnNvbWV0aW1lc0J5KC43LCB4ID0%2BIHguc2xvdyhzaW5lLnNsb3coMSkucmFuZ2UoMCwxKSkpCiAgICAsCiAgICAvLyBzKCJlYXN0KjIiKS5uKGlyYW5kKDYpKS5nYWluKC40KSwKICAgIHMoImVhc3Q6NigzLDgpIikuZ2FpbihzaW5lLnJhbmdlKC4xLC4zKS5zbG93KDEpKSwKICAgIC8vIG5vdGUoImI1IikuZXVjbGlkUm90KDMsMTYsMTQpLmNsaXAoc2F3LnNsb3coMikpLmdhaW4oc3F1YXJlLnNsb3coMykucmFuZ2UoLjEsLjUpKS5kZWdyYWRlQnkoLjIpLAogICAgcygiaGgqOCIpLm9mZigxLzMyLCB4PT54LmFkZCgxKSkuYmFuaygiUm9sYW5kVFI5MDkiKS5nYWluKHBlcmxpbi5yYW5nZSgwLC4xKSkuZGVncmFkZUJ5KC4zKSwgLy9iZDozKDMsOCkKICAgIHMoImJkOjMoMyw4KSIpLmJhbmsoIlJvbGFuZFRSOTA5IikuZ2FpbihwZXJsaW4ucmFuZ2UoMCwuMSkpLmRlZ3JhZGVCeSguMyksCiAgKQogIC5zbG93KDMpCiAgLnBpYW5vcm9sbCh7dmVydGljYWw6MX0pCiAgLy8gLmh1c2goKQogIA%3D%3D
-/***********************************************************************/
+// https://strudel.cc/#JDogc3RhY2soCnMoIndoaXRlIHwgcGluayB8IGJyb3duIikuYWRzcigiLjE6LjE6MDouMiIpLnN1c3RhaW4ocGVybGluLnJhbmdlKC40LCAuNikpLmdhaW4oLjMpCiAgICAuc29tZXRpbWVzQnkoLjcsIHggPT4geC5zbG93KHNpbmUuc2xvdygxKS5yYW5nZSgwLDEpKSkKLAovLyBzKCJlYXN0KjIiKS5uKGlyYW5kKDYpKS5nYWluKC40KSwKLy8gcygiZWFzdDo2KDMsOCkiKS5nYWluKHNpbmUucmFuZ2UoLjEsLjMpLnNsb3coMSkpLAovL25vdGUoImI1IikuZXVjbGlkUm90KDMsMTYsMTQpLmNsaXAoc2F3LnNsb3coMikpLmdhaW4oc3F1YXJlLnNsb3coMykucmFuZ2UoLjEsLjUpKS5kZWdyYWRlQnkoLjIpLAovLyBzKCJoaCo4Iikub2ZmKDEvMzIsIHg9PnguYWRkKDEpKS5iYW5rKCJSb2xhbmRUUjkwOSIpLmdhaW4ocGVybGluLnJhbmdlKDAsLjEpKS5kZWdyYWRlQnkoLjMpLCAvL2JkOjMoMyw4KQovLyBzKCJiZDozKDMsOCkiKS5iYW5rKCJSb2xhbmRUUjkwOSIpLmdhaW4ocGVybGluLnJhbmdlKDAsLjEpKS5kZWdyYWRlQnkoLjMpLAopCi5zbG93KDMpCi5wdW5jaGNhcmQoe3ZlcnRpY2FsOjAsIGxhYmVsczoxfSkuY29sb3IoImJsdWUiKQouX3NwZWN0cnVtKCkKLy8gLmh1c2goKQ%3D%3D
 stack(
 s("white | pink | brown").adsr(".1:.1:0:.2").sustain(perlin.range(.4, .6)).gain(.3)
     .sometimesBy(.7, x => x.slow(sine.slow(1).range(0,1)))
@@ -11,5 +10,7 @@ s("hh*8").off(1/32, x=>x.add(1)).bank("RolandTR909").gain(perlin.range(0,.1)).de
 s("bd:3(3,8)").bank("RolandTR909").gain(perlin.range(0,.1)).degradeBy(.3),
 )
 .slow(3)
-.pianoroll({vertical:1})
+.color("blue")
+.punchcard({vertical:0, labels:1})
+._spectrum()
 // .hush()
