02c7b89469f6f3d3e3ac2382e527f308be0e4640
bodobraegger <braegger.bodo@gmail.com>
2025-05-05 16:54:48 +0200

    moodboard
    ;

--- 15.09.25-daz-kunsthaus.js
+++ 15.09.25-daz-kunsthaus.js
@@ -7,21 +7,57 @@ loadScript(extension_location + "hydra-canvas.js")
 loadScript(extension_location + "hydra-tap.js")
 loadScript(extension_location + "hydra-wrap.js")
 
+a.hide()
+
+hush()
 
 canvas.setRelativeSize(1);
-bpm = 125
+bpm = 140
 
 s0.initScreen(2)
 
 s1.initScreen(3)
 
+
+// jellyfish
+osc(Math.PI * 2, .2, 1)
+  .color(1, -.5, 1)
+  .mask(shape([3,4,8,99].smooth(.5), .1, .5))
+  .scale(1, innerHeight / innerWidth)
+  .modulate(noise([...Array(5).keys()].smooth(1).fast(.2)))
+  .modulateScale(osc(20, .01), -.5)
+  .blend(src(o2)
+    .scrollY(-0.1), 0.2)
+  .modulate(o2, () => .1 * Math.cos(time))
+  .colorama([0, .02].smooth(1))
+  .saturate(() => am(1, .2, 0))
+  .luma(() => am(0.008, .01, 2), 0)
+  .blend(o2, .9)
+  .scale(() => am(1.01, -.02, 2))
+  .out(o2)
+
+// audio layer
+src(o0).modulate(o0).pixelate([...Array(4).fill(innerWidth),100].smooth(.1),[...Array(4).fill(innerHeight),50].smooth(.01))
+  .add(src(o1).pixelate([...Array(4).fill(innerWidth),1000,500,100,50,12].smooth(.1),[...Array(4).fill(innerHeight),1000,500,100,12].smooth(.8).fast(.25)),[0.1,0.2].smooth())
+  .diff(shape(2.5).scale(()=>am(0.2,0.9)).rotate(0,.1)
+  .repeat(()=>am(7,3,0),()=>am(8,1,numBins),()=>am(1,1,0),()=>am(1,1,2))
+  .modulate(noise()).modulate(o0,()=>am(.2,1)).color([1,0,0].smooth(.1).fast(.0125),[0,.5,0].smooth(.1).fast(.0126),[0,0,1].fast(.0124).smooth(.1))
+  .add(o2,0.01)
+  .pixelate([2000, 1000, 500, 50, 10].fast(1/7),2000))
+  .out(o3)
+
+
+// background
 var m0 = osc([240,120,60,30,15,8]).thresh().rotate(0,.1).pixelate()
 var m1 = osc([240,120,60,30,15,8]).thresh().rotate(0,.1).pixelate()
 .invert()
-m0.out(o1)
-m1.out(o2)
-src(s1).mask(m1)
-  .add(src(s0).mask(m0))
-  .out()
+var c1=[0.5, 1].fast(1/4).smooth(), c2=0.5, c3=1.1
+src(s0).color(c1,c2,c3)
+.mult(m1,[0.5,1].smooth().fast(1/7))
+.add(src(s1).color(c1,c2,c3).mask(m2))
+.modulate(s0, [0,0.01,0.1,0.01].fast(1/4).smooth())
+.out()
 
-render(o0)
+speed = 0.5
+
+render(o3)
